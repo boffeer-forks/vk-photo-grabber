2629ac2059014c72fe96929b88a410201282d129
Yevhen Hrytsai <yevgen.grytsay@gmail.com>
2018-06-22 19:47:08 +0300

    Producer and consumer examples

---
+++ app/examples/consumer.php
@@ -0,0 +1,30 @@
+<?php
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+use Enqueue\AmqpLib\AmqpConnectionFactory;
+
+$connectionFactory = new AmqpConnectionFactory([
+    'host'  => 'localhost',
+    'port'  => 5672,
+    'vhost' => '/',
+    'user'  => 'admin',
+    'pass'  => 'qwe123',
+//    'persisted' => false,
+]);
+
+/** @var \Enqueue\AmqpLib\AmqpContext $psrContext */
+$psrContext = $connectionFactory->createContext();
+
+$destination = $psrContext->createQueue('foo');
+$psrContext->declareQueue($destination);
+//$destination = $context->createTopic('foo');
+
+$consumer = $psrContext->createConsumer($destination);
+
+$message = $consumer->receive();
+
+var_dump($message);
+// process a message
+
+$consumer->acknowledge($message);
+// $consumer->reject($message);
---
+++ app/examples/producer.php
@@ -0,0 +1,26 @@
+<?php
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+use Enqueue\AmqpLib\AmqpConnectionFactory;
+
+//define('AMQP_DEBUG', true);
+$connectionFactory = new AmqpConnectionFactory([
+    'host'  => 'localhost',
+    'port'  => 5672,
+    'vhost' => '/',
+    'user'  => 'admin',
+    'pass'  => 'qwe123',
+//    'persisted' => false,
+]);
+
+
+/** @var \Enqueue\AmqpLib\AmqpContext $psrContext */
+$psrContext = $connectionFactory->createContext();
+
+$destination = $psrContext->createQueue('foo');
+$psrContext->declareQueue($destination);
+//$destination = $psrContext->createTopic('foo');
+
+$message = $psrContext->createMessage('Hello world!');
+
+$psrContext->createProducer()->send($destination, $message);
